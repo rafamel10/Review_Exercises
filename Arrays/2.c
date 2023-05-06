1cb9d855bd89a27d13cd189c2ba374a4bea06915
Rafael Barros <118626138+rafamel10@users.noreply.github.com>
2023-05-06 18:46:15 -0300

    Submetendo ex2

---
+++ Arrays/2.c
@@ -0,0 +1,16 @@
+/*Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.*/
+#include <stdio.h>
+
+int main(){
+    float Vet[10];
+    int i;
+    for(i=0;i<10;i++){
+        printf("Digite o %i° valor: ",i+1);
+        scanf("%f",&Vet[i]);
+    }
+    for(i=9;i>=0;i--){
+        printf("[%.2f]",Vet[i]);
+    }
+    return 0;
+
+}
